762225021d6cf1ea3c46a7694f8e190fea716f41
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-05-13 14:40:35 -0400

    moved length to __len__

--- structures/basenum.py
+++ structures/basenum.py
@@ -7,8 +7,7 @@ class BaseNum:
   def __repr__(self):
     return self.data
 
-  # @return [Integer] The length of the number
-  def length(self):
+  def __len__(self):
     return len(self.data)
 
   # @param [Integer] The index of the digit to fetch
@@ -20,14 +19,14 @@ class BaseNum:
   # @param pad [String] The pad to use. Default is '0'.
   # @return [BaseNum] The number of pads to prepend.
   def pad(self, length, pad='0'):
-    return self.__class__((length - self.length()) * pad + self.data)
+    return self.__class__((length - len(self)) * pad + self.data)
 
   # @param str_1 [BaseNum] The first string to make of equal length
   # @param str_2 [BaseNum] The second string to make of equal length
   # @return [BaseNum, BaseNum] The two strings, now of equal length
   @staticmethod
   def normalize_length(str_1, str_2):
-    if str_1.length() < str_2.length():
-        return str_1.pad(str_2.length()), str_2
-    elif str_2.length() <= str_1.length():
-        return str_1, str_2.pad(str_1.length())
+    if len(str_1) < len(str_2):
+        return str_1.pad(len(str_2)), str_2
+    elif len(str_2) <= len(str_1):
+        return str_1, str_2.pad(len(str_1))
