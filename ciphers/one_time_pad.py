352ac13781592f821ecc38726286b065ce2e0289
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-05-15 01:11:14 -0400

    removed some junk

--- ciphers/one_time_pad.py
+++
@@ -1,5 +0,0 @@
-# @param text [HexNum] The text to encrypt/decrypt.
-# @param key [HexNum] The key to encrypt/decrypt with.
-# @return [HexNum] The ciphertext/plaintext
-def one_time_pad(text, key):
-  return text ^ key.repeat(len(text))
