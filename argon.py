c0e33a88f99778fa4cc47f08965e405994006c36
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-05-18 15:00:07 -0400

    fixed binnum/hexnum import issue

--- argon.py
+++ argon.py
@@ -1,6 +1,5 @@
-# TODO: Fix the import order issue here
-from structures.hexnum import HexNum
 from structures.binnum import BinNum
+from structures.hexnum import HexNum
 from prngs.rc4 import RC4
 
 a = BinNum('1111000011110000')
