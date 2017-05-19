f58d5c0169d0c2b3080a24bfb0d741485c7fc3a2
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-05-19 14:00:40 -0400

    added skeleton for RC4A

--- argon.py
+++ argon.py
@@ -3,6 +3,4 @@ from structures.hexnum import HexNum
 from prngs.rc4 import RC4
 
 a = BinNum('1111000011110000').append(BinNum('01'))
-print a.get_byte(1)
-print a.bytes()
-print a
+print RC4(a, 10)
--- prngs/rc4.py
+++ prngs/rc4.py
@@ -1,9 +1,27 @@
 from structures.basenum import BaseNum
 from utils.constants import Constants
 
+# @param seed [BaseNum] The seed to use for the key-scheduing.
+# @return [Array<Integer>] The result of the RC4 key-scheduling algorithm
+def __key_scheduling(seed):
+  key = range(Constants.MAX_BYTE)
+  shuffle = 0
+  for index in xrange(Constants.MAX_BYTE):
+    byte = seed.get_byte(index % seed.bytes()).to_integer()
+    shuffle = (shuffle + key[index] + byte) % Constants.MAX_BYTE
+    key[index], key[shuffle] = key[shuffle], key[index]
+  return key
+
 # @param seed [BaseNum] The seed to use RC4's PRNG with.
 # @param bits [Integer] The number of bits to return.
 # @return [BaseNum] The first n-bits of RC4's output for the given seed.
 def RC4(seed, bits):
+  # TODO: Finish this.
+  pass
+
+# @param seed [BaseNum] The seed to use RC4A's PRNG with.
+# @param bits [Integer] The number of bits to return.
+# @return [BaseNum] The first n-bits of RC4A's output for the given seed.
+def RC4A(seed, bits):
   # TODO: Finish this.
   pass
