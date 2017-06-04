089490f89c92ee4e3471f21e0c4b0233e97adb07
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-04 15:43:02 -0400

    upgraded build_from_prng function

--- ciphers/streamcipher.py
+++ ciphers/streamcipher.py
@@ -4,23 +4,24 @@ class StreamCipher(Cipher):
   def __init__(self, key):
     super(StreamCipher, self).__init__(key)
 
-  # @param text [BaseNum] The plaintext to encrypt
-  # @return [BaseNum] The encrypted ciphertext
+  # @param text [BaseNum] The plaintext to encrypt.
+  # @return [BaseNum] The encrypted ciphertext.
   def encrypt(self, text):
     return text ^ self.prng.generate_output(len(text))
 
-  # @param text [BaseNum] The ciphertext to decrypt
-  # @return [BaseNum] The plaintext decrypted
+  # @param text [BaseNum] The ciphertext to decrypt.
+  # @return [BaseNum] The plaintext decrypted.
   def decrypt(self, text):
     return text ^ self.prng.generate_output(len(text))
 
-  # @param prng [PRNG] The PRNG to use for the cipher
-  # @param key [BaseNum] The key to use for the cipher
-  # @return [StreamCipher] A cipher using the given PRNG
+  # @param prng [PRNG] The PRNG to use for the cipher.
+  # @param key [BaseNum] The key to use for the cipher.
+  # @return [StreamCipher] A cipher using the given PRNG.
+  # @raise [ValueError] If an invalid PRNG is given.
   @staticmethod
   def build_from_prng(prng, key):
-    if prng.__name__ == 'RC4':
-      from ciphers.rc4 import RC4
-      return RC4(key)
-    else:
-      raise ValueError('Unknown PRNG given.')
+    from ciphers.rc4 import RC4
+    for klass in StreamCipher.__subclasses__():
+      if klass.__name__ == prng.__name__:
+        return klass(key)
+    raise ValueError('Unknown PRNG given.')
