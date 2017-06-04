beb4ae9d2f93ab5eb01be366c8a08f696ffa5330
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-04 02:40:46 -0400

    removed pointless overridden functions in RC4 cipher

--- ciphers/rc4.py
+++ ciphers/rc4.py
@@ -5,13 +5,3 @@ class RC4(StreamCipher):
   def __init__(self, key):
     super(RC4, self).__init__(key)
     self.prng = RC4PRNG(key)
-
-  # @param text [BaseNum] The plaintext to encrypt
-  # @return [BaseNum] The encrypted ciphertext
-  def encrypt(self, text):
-    return text ^ self.prng.generate_output(len(text))
-
-  # @param text [BaseNum] The ciphertext to decrypt
-  # @return [BaseNum] The plaintext decrypted
-  def decrypt(self, text):
-    return text ^ self.prng.generate_output(len(text))
