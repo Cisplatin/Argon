bfb8a43b7d66375073f2bf4a4a3e12f47eba00d4
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-06 01:23:09 -0400

    added length check for idempotent prng

---
+++ prngs/idempotent.py
@@ -0,0 +1,23 @@
+from structures.basenum import BaseNum
+from prngs.prng import PRNG
+from utils.constants import Constants
+
+class Idempotent(PRNG):
+  def __init__(self, seed):
+    super(Idempotent, self).__init__(seed)
+    self.bit = 0
+
+  # @note Resets the PRNG to start generating outputs from the beginning.
+  def reset(self):
+    self.bit = 0
+
+  # @param bits [Integer] The number of bits to return.
+  # @return [BaseNum] The next n-bits of the given seed.
+  # @raise [ValueError] If there are not enough bits left.
+  def generate_output(self, bits):
+    # TODO Cleaner .data call
+    if self.bits + bits >= len(self.seed.data):
+      raise ValueError('Not enough of the key left to encrypt.')
+    result = self.seed.__class__(self.seed.data[self.bits:self.bits + bits])
+    self.bits += bits
+    return result
