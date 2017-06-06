c8456dbf59819311cf8bae93c7a1d77988583790
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-06 01:20:14 -0400

    added idempotetency PRNG

--- argon.py
+++ argon.py
@@ -1,13 +1,19 @@
 from structures.binnum import BinNum
 from prngs.rc4 import RC4
+from prngs.idempotent import Idempotent
 from ciphers.streamcipher import StreamCipher
 
 key = BinNum('1001001')
 prng = RC4(key)
 
-cipher = StreamCipher.build_from_prng(RC4, key)
-print cipher.encrypt(key)
-print cipher.encrypt(key)
-cipher.reset()
+cipher = Idempotent(key)
+print cipher.generate_output(2)
+print cipher.generate_output(10)
+print cipher.generate_output(10)
 
-print cipher.encrypt(key)
+#cipher = StreamCipher.build_from_prng(RC4, key)
+#print cipher.encrypt(key)
+#print cipher.encrypt(key)
+#cipher.reset()
+
+#print cipher.encrypt(key)
