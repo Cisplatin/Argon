031ee142f6807e6b73da6f37d7c5edf72b83591f
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-20 20:02:50 -0400

    added clean_oct_string for octal instantiation

--- structures/octnum.py
+++ structures/octnum.py
@@ -7,3 +7,17 @@ class OctNum(BaseNum):
   FORMAT = '{0:o}'
   ENCODING = 'oct'
   REGEX = r'^[0-7]*$'
+
+  # @param data [String] The value to represent by the OctNum.
+  # @raise [ValueError] If the given value is not valid octal.
+  def __init__(self, data):
+    data = OctNum.clean_oct_string(data)
+    super(OctNum, self).__init__(data)
+
+  # @param string [String] The string to remove the 0 prefix from.
+  # @return [String] The string without the 0 prefix.
+  @staticmethod
+  def clean_oct_string(string):
+    if string.startswith('0'):
+      return string[len('0'):]
+    return string
